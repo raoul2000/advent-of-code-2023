bbc1a0d2ee360f2ca99a9ac2fefaf04337527ada
raoul2000 <manu34@free.fr>
2023-12-13 17:15:09 -0300

    day_4 part 2 - solved

--- src/day_4.clj
+++ src/day_4.clj
@@ -138,6 +138,178 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
 
   (solution-1 (slurp "resources/day_4.txt"))
   ;; => 535235 .. and one more ⭐
-  
+
   ;;
+  )
+
+;;;; part 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; This time, processing the card in sequence, 
+;; for each card, create copies of the n following card, where n is th number of points hold by this
+;; card, following part 1.
+
+(comment
+
+  ;; To Find the value for each card, we apply the same processing as in part one
+  ;; except that we don't compute final solution.  
+
+  (def card-value (->> (s/split-lines sample-input)
+                       (map create-card-1)
+                       (map (fn [[winnings played]]
+                              (set/intersection winnings played)))
+                       #_(filter seq)
+                       (mapv count)))
+
+  ;; for sample input we get [4 2 2 1]
+  ;; card 0 holds 4 points,
+  ;; card 1 holds 2 points 
+  ;; etc ...
+
+  ;; Note that actual card numbers starts with 1 in the puzzle input data
+  ;; but to simplify we'll translate to zero based index. 
+
+  ;; To get value of card 0 (card n°1 in the input data)
+  (get card-value 0)
+  ;; To get value of card 1 (card n°2 in the input data)
+  (get card-value 1)
+
+  ;; Now we must store how many card are there of each number.
+  ;; With sample data we have at the begining : 
+  ;; [1 1 1 1 1 1] : one card of each
+
+  ;; Processing card 0 : it has a value of 4 so all 4 card following 0 must be duplicated.
+  ;; This gives :
+  ;; [1 2 2 2 2 1]
+  ;;  ^
+
+  ;; Processing card 1 with value of 2: the 2 card after card 1 must be duplicated but as we have
+  ;; 2 cards 1, 2 following card must be duplicated onece more
+  ;; - add 2 to the 2 following numbers :
+  ;; [1 2 4 4 2 1]  
+  ;;    ^
+
+  ;; Processing card 2 with value of 2: the 2 card after card 2 must be duplicated but as we have
+  ;; 4 cards 2, 2 following card must be duplicated twice more
+  ;; - add 4 to the 2 following numbers:
+  ;; [1 2 4 8 6 1]  
+  ;;      ^
+
+  ;; Processing card 3 with value of 1: the 1 card after card 3 must be duplicated but as we have
+  ;; 8 cards 3, the following card must be duplicated in total 8
+  ;; - add 8 to the 1 following number:
+  ;; [1 2 4 8 14 1]
+  ;;        ^
+
+  ;; Processing card 4 with value of 0: no change
+  ;; This gives :
+  ;; [1 2 4 8 14 1]
+  ;;          ^
+
+  ;; Processing card 5 with value of 0: no change
+  ;; This gives :
+  ;; [1 2 4 8 14 1]
+  ;;             ^
+
+  ;; In the end we have [1 2 4 8 14 1]. 
+  ;; Sum them up to get the part 2 solution (30 for sample input)
+
+
+  ;; Naming things :
+  ;; - idx      : zero based card number
+  ;; - cnt      : card count
+  ;; - card-val : card value
+
+  ;; initially, each card only appears once. It muse be a vector
+  ;; because we want to access it via index.
+  ;; Working on the sample input, we have 6 cards
+
+  (def card-count (vec (repeat 6 1)))
+
+  ;; ... and we want to process all cards
+  (def card-indexes (range 0 (count card-count)))
+
+  (reduce (fn [acc idx]
+            (let [cnt      (get acc idx)
+                  card-val (get card-value idx)]
+              (tap> [cnt card-val])
+              (if (zero? card-val)
+                acc
+                acc ;; do something clever here
+                )))card-count  card-indexes)
+
+
+  ;; Maybe it's time to see how we can work on the card-count array to update a
+  ;; slice of it.
+
+  (defn apply-to-slice
+    "Apply *f* to *n* items in *v* after position *start*"
+    [start n f v]
+    (vec (map-indexed (fn [idx val-idx]
+                        (cond-> val-idx
+                          (> (+ start (inc n)) idx start) f))  v)))
+
+  (apply-to-slice 0 2 inc [1 1 1 1 1])
+  (apply-to-slice 1 2 inc [1 1 1 1 1])
+  (apply-to-slice 2 2 inc [1 1 1 1 1])
+  (apply-to-slice 3 2 inc [1 1 1 1 1])
+  (apply-to-slice 4 2 inc [1 1 1 1 1])
+
+  ;; Now we can complete our reduce expression
+  (reduce (fn [acc idx]
+            (let [cnt      (get acc idx)
+                  card-val (get card-value idx)]
+              (if (zero? card-val)
+                acc
+                (apply-to-slice idx card-val (partial + cnt) acc)))) card-count  card-indexes)
+
+  ;; For sample input this returns [1 2 4 8 14 1] and when added, 30 which is the
+  ;; expected result.
+
+  ;; Let's clean up and write function to solve part 2
+  )
+
+(defn compute-card-values [input]
+  (->> (s/split-lines input)
+       (map create-card-1)
+       (map (fn [[winnings played]]
+              (set/intersection winnings played)))
+       (mapv count)))
+
+(comment
+  (compute-card-values sample-input)
+  ;;
+  )
+
+(defn apply-to-slice
+  "Apply *f* to *n* items in *v* after position *start*"
+  [start n f v]
+  (vec (map-indexed (fn [idx val-idx]
+                      (cond-> val-idx
+                        (> (+ start (inc n)) idx start) f))  v)))
+(comment
+  (apply-to-slice 0 2 inc [1 1 1 1 1])
+  ;; 
+  )
+
+(defn solution-2 [input]
+  (let [card-values      (compute-card-values input)
+        card-count       (vec (repeat (count card-values) 1))
+        card-indexes     (range 0 (count card-values))
+        card-count-final (reduce (fn [acc idx]
+                                   (let [cnt      (get acc idx)
+                                         card-val (get card-values idx)]
+                                     (if (zero? card-val)
+                                       acc
+                                       (apply-to-slice idx card-val (partial + cnt) acc)))) card-count  card-indexes)]
+    (reduce + card-count-final)))
+
+(comment
+  ;; with sample input
+  (solution-2 sample-input)
+  ;; => 30 ok
+
+  ;; with puzzle input ...
+  (solution-2 (slurp "resources/day_4.txt"))
+  ;; Yuuupiii !! one more !! ⭐ 
   )
